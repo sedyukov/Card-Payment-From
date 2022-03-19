99b9fcc8df89c630c9db596540e4667ba4514181
sedyukov <62834007+sedyukov@users.noreply.github.com>
2022-03-19 17:59:23 +0300

    Main files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/antd.css";
+import DrawerForm from "./DrawerForm";
+import { Layout, Typography } from "antd";
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout">
+      <Header>
+        <Title
+          style={{ color: "white", margin: "10px", minHeight: "20vh" }}
+          level={2}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Payment Form
+        </Title>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <div
+          style={{
+            background: "#fff",
+            minHeight: "80vh",
+            textAlign: "center",
+            paddingTop: "40px",
+          }}
+        >
+          <DrawerForm />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Test case by Sedyukov Yaroslav
+      </Footer>
+    </Layout>
   );
 }
 
